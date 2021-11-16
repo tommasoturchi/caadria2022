b62c713d0f42960ceb08e33bd3609d234730755d
Tommaso Turchi <apps@tommasoturchi.fyi>
2021-11-16 15:31:33 +0100

    disabled button on initial loading

--- src/Prediction.js
+++ src/Prediction.js
@@ -16,8 +16,6 @@ const InfoBox = styled(Paper)(({ theme }) => ({
 }));
 
 export function Prediction() {
-  // eslint-disable-next-line no-unused-vars
-  const [initModel] = useAtom(modelAtom);
   const [{ resilience, clusters }] = useAtom(predictionAtom);
 
   return (
@@ -27,22 +25,30 @@ export function Prediction() {
           <InfoBox>
             <Typography variant="subtitle2" component="div" color="primary">
               {`Buildings: ${
-                clusters == null || clusters[0].length === 0 ? "—" : clusters[0].map(c => `${c.toFixed(2)}km`).join(", ")
+                clusters == null || clusters[0].length === 0
+                  ? "—"
+                  : clusters[0].map((c) => `${c.toFixed(2)}km`).join(", ")
               }`}
             </Typography>
             <Typography variant="subtitle2" component="div" color="primary">
               {`Green Areas: ${
-                clusters == null || clusters[1].length === 0 ? "—" : clusters[1].map(c => `${c.toFixed(2)}km`).join(", ")
+                clusters == null || clusters[1].length === 0
+                  ? "—"
+                  : clusters[1].map((c) => `${c.toFixed(2)}km`).join(", ")
               }`}
             </Typography>
             <Typography variant="subtitle2" component="div" color="primary">
               {`Infrastructures: ${
-                clusters == null || clusters[2].length === 0 ? "—" : clusters[2].map(c => `${c.toFixed(2)}km`).join(", ")
+                clusters == null || clusters[2].length === 0
+                  ? "—"
+                  : clusters[2].map((c) => `${c.toFixed(2)}km`).join(", ")
               }`}
             </Typography>
             <Typography variant="subtitle2" component="div" color="primary">
               {`Natural Elements: ${
-                clusters == null || clusters[3].length === 0 ? "—" : clusters[3].map(c => `${c.toFixed(2)}km`).join(", ")
+                clusters == null || clusters[3].length === 0
+                  ? "—"
+                  : clusters[3].map((c) => `${c.toFixed(2)}km`).join(", ")
               }`}
             </Typography>
           </InfoBox>
@@ -60,16 +66,22 @@ export function Prediction() {
 }
 
 export function MakePrediction() {
+  const [initModel] = useAtom(modelAtom);
   const [{ loading }, predict] = useAtom(predictionAtom);
 
   const handleButtonClick = useCallback(() => predict(), [predict]);
 
   return (
     <Box sx={{ m: 1, position: "absolute", bottom: "2rem", right: "4rem" }}>
-      <Fab aria-label="predict" color="primary" onClick={handleButtonClick}>
+      <Fab
+        aria-label="predict"
+        color="primary"
+        disabled={loading || initModel == null}
+        onClick={handleButtonClick}
+      >
         <PredictionIcon />
       </Fab>
-      {loading && (
+      {(loading || initModel == null) && (
         <CircularProgress
           size={68}
           color="secondary"
