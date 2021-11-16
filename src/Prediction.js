458d950dcaad6d6000dfd2002e92f0cd6282cdb1
Tommaso Turchi <apps@tommasoturchi.fyi>
2021-11-16 15:21:08 +0100

    trained model + computed resilience

--- src/Prediction.js
+++ src/Prediction.js
@@ -11,7 +11,6 @@ import { useAtom } from "jotai";
 import { useCallback } from "react";
 
 const InfoBox = styled(Paper)(({ theme }) => ({
-  textAlign: "center",
   padding: theme.spacing(1),
   backgroundColor: "#121212",
 }));
@@ -19,16 +18,44 @@ const InfoBox = styled(Paper)(({ theme }) => ({
 export function Prediction() {
   // eslint-disable-next-line no-unused-vars
   const [initModel] = useAtom(modelAtom);
-  const [{ result }] = useAtom(predictionAtom);
+  const [{ resilience, clusters }] = useAtom(predictionAtom);
 
   return (
-    <Box sx={{ m: 1, position: "absolute", bottom: "2rem", left: "2rem" }}>
-      <InfoBox>
-        <Typography variant="h6" component="div" color="primary">
-          {`Resilience: ${result}`}
-        </Typography>
-      </InfoBox>
-    </Box>
+    <>
+      {clusters != null && (
+        <Box sx={{ m: 1, position: "fixed", bottom: "6rem", left: "2rem" }}>
+          <InfoBox>
+            <Typography variant="subtitle2" component="div" color="primary">
+              {`Buildings: ${
+                clusters == null || clusters[0].length === 0 ? "—" : clusters[0].map(c => `${c.toFixed(2)}km`).join(", ")
+              }`}
+            </Typography>
+            <Typography variant="subtitle2" component="div" color="primary">
+              {`Green Areas: ${
+                clusters == null || clusters[1].length === 0 ? "—" : clusters[1].map(c => `${c.toFixed(2)}km`).join(", ")
+              }`}
+            </Typography>
+            <Typography variant="subtitle2" component="div" color="primary">
+              {`Infrastructures: ${
+                clusters == null || clusters[2].length === 0 ? "—" : clusters[2].map(c => `${c.toFixed(2)}km`).join(", ")
+              }`}
+            </Typography>
+            <Typography variant="subtitle2" component="div" color="primary">
+              {`Natural Elements: ${
+                clusters == null || clusters[3].length === 0 ? "—" : clusters[3].map(c => `${c.toFixed(2)}km`).join(", ")
+              }`}
+            </Typography>
+          </InfoBox>
+        </Box>
+      )}
+      <Box sx={{ m: 1, position: "fixed", bottom: "2rem", left: "2rem" }}>
+        <InfoBox>
+          <Typography variant="h6" component="div" color="primary">
+            {`Resilience: ${resilience == null ? "—" : resilience.toFixed(2)}`}
+          </Typography>
+        </InfoBox>
+      </Box>
+    </>
   );
 }
 
--- src/atoms.js
+++ src/atoms.js
@@ -3,7 +3,11 @@ import * as tf from "@tensorflow/tfjs";
 import { atom } from "jotai";
 
 tf.ready().then(() => {
-  tf.setBackend("cpu");
+  if (
+    navigator.userAgent.indexOf("Chrome") > -1 ||
+    navigator.userAgent.indexOf("Firefox") > -1
+  )
+    tf.setBackend("cpu");
 }); // webgl2 is buggy on chrome
 
 export const zoomAtom = atom(parseFloat(process.env.REACT_APP_DEFAULT_ZOOM));
@@ -13,8 +17,6 @@ export const centerAtom = atom({
   lng: parseFloat(process.env.REACT_APP_DEFAULT_LNG),
 });
 
-// !python export.py --weights /content/yolov5/runs/train/yolov5s_results2/weights/best.pt --include tflite
-// !python export.py --weights /content/yolov5/runs/train/yolov5s_results2/weights/best.pt --include tfjs
 export const modelAtom = atom(null);
 modelAtom.onMount = (setModel) => {
   const run = async () => {
@@ -25,10 +27,40 @@ modelAtom.onMount = (setModel) => {
   run();
 };
 
+const cropToCanvas = (image, canvas, ctx) => {
+  const naturalWidth = image.naturalWidth;
+  const naturalHeight = image.naturalHeight;
+
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  const ratio = Math.min(
+    canvas.width / image.naturalWidth,
+    canvas.height / image.naturalHeight
+  );
+  const newWidth = Math.round(naturalWidth * ratio);
+  const newHeight = Math.round(naturalHeight * ratio);
+  ctx.drawImage(
+    image,
+    0,
+    0,
+    naturalWidth,
+    naturalHeight,
+    (canvas.width - newWidth) / 2,
+    (canvas.height - newHeight) / 2,
+    newWidth,
+    newHeight
+  );
+};
+
 const predictionResultAtom = atom({
   loading: false,
   error: null,
-  result: "—",
+  resilience: null,
+  clusters: null,
 });
 export const predictionAtom = atom(
   (get) => get(predictionResultAtom),
@@ -48,27 +80,69 @@ export const predictionAtom = atom(
           const ctx = canvas.getContext("2d");
           canvas.height = this.height;
           canvas.width = this.width;
-          ctx.drawImage(this, 0, 0);
+          cropToCanvas(img, canvas, ctx);
 
           const imageData = ctx.getImageData(0, 0, this.width, this.height);
-          const input = tf.image
-            .resizeBilinear(tf.browser.fromPixels(imageData), [
-              parseInt(process.env.REACT_APP_IMG_SIZE),
-              parseInt(process.env.REACT_APP_IMG_SIZE),
-            ])
-            .div(255.0)
-            .expandDims(0);
+          const input = tf.tidy(() =>
+            tf.image
+              .resizeBilinear(tf.browser.fromPixels(imageData), [
+                parseInt(process.env.REACT_APP_IMG_SIZE),
+                parseInt(process.env.REACT_APP_IMG_SIZE),
+              ])
+              .div(255.0)
+              .expandDims(0)
+          );
 
           model.executeAsync(input).then((res) => {
-            const [, , , valid_detections] = res;
+            const [boxes, , classes, valid_detections] = res;
+            const boxes_data = boxes.dataSync();
+            const classes_data = classes.dataSync();
             const valid_detections_data = valid_detections.dataSync()[0];
 
             tf.dispose(res);
 
+            let resilience = 0;
+            let clusters = [[], [], [], []];
+            for (let c = 0; c < valid_detections_data; ++c) {
+              const [x1, y1, x2, y2] = boxes_data.slice(c * 4, (c + 1) * 4);
+              const boxCenter = [(x1 + x2) / 2, (y1 + y2) / 2];
+
+              const pixelDistance =
+                Math.sqrt(
+                  Math.pow(boxCenter[0] - 0.5, 2) +
+                    Math.pow(boxCenter[1] - 0.5, 2)
+                ) * canvas.width;
+
+              const kmDistance =
+                (pixelDistance *
+                  156543.03392 *
+                  Math.cos((center.lat * Math.PI) / 180)) /
+                Math.pow(2, zoom) /
+                1000;
+
+              let gamma = 0;
+              switch (classes_data[c]) {
+                case 2:
+                  if (kmDistance <= 12.5) gamma = 1;
+                  else if (kmDistance <= 25) gamma = 0.5;
+                  else gamma = 0.25;
+                  break;
+                default:
+                  if (kmDistance <= 1.6) gamma = 1;
+                  else if (kmDistance <= 2.4) gamma = 0.75;
+                  else if (kmDistance <= 8) gamma = 0.5;
+                  else gamma = 0.25;
+              }
+
+              resilience += kmDistance * gamma;
+              clusters[classes_data[c]].push(kmDistance);
+            }
+
             set(predictionResultAtom, {
               loading: false,
               error: null,
-              result: valid_detections_data,
+              resilience,
+              clusters,
             });
           });
         };
